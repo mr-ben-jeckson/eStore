f3a29e8d6d6550c23a4d68ceac3e7d100a425b4a
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-15 19:50:49 +0630

    cron set to 8:30 PM

--- controllers/app.js
+++ controllers/app.js
@@ -1,9 +1,9 @@
 const nodeCron = require('node-cron');
    
-const jobs = nodeCron.schedule('0 */3 * * * *', () => {
-        /* Every Day Jobs Here at 7 PM Asia/Yangon Time */ 
+const jobs = nodeCron.schedule('30 20 * * *', () => {
+        /* Every Day Jobs Here at 8:30 PM Asia/Yangon Time */ 
         /* Your work is here */
-        console.log('Running Job At 7 PM at Myanmar/Yangon');
+        console.log('Running Job At 8:30 PM at Myanmar/Yangon');
     },{
         scheduled: true,
         timezone: "Asia/Yangon" // Myanmar Time Zone
