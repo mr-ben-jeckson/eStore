ed95f1f64de3fe1910b345bc513f94dac08c7900
Ben Jeckson <thanhtikexaw@gmail.com>
2023-03-15 18:05:15 +0630

    corn changed

--- controllers/app.js
+++ controllers/app.js
@@ -1,10 +1,10 @@
 const corn = require('node-cron');
 
 const jobs = () => {
-    /* Every Day Jobs Here at 9 AM Asia/Yangon Time */
-    corn.schedule('0 9 * * *', () => {
+    /* Every Day Jobs Here at 7 PM Asia/Yangon Time */
+    corn.schedule('0 19 * * *', () => {
         /* Your work is here */
-        console.log('Running Job At 9 AM at Myanmar/Yangon');
+        console.log('Running Job At 7 PM at Myanmar/Yangon');
     },{
         scheduled: true,
         timezone: "Asia/Yangon" // Myanmar Time Zone
